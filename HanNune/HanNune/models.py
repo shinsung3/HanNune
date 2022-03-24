1705303ce625ee776d6bf7a8535d956ca4f099ab
subin97 <s979769@icloud.com>
2022-03-24 13:29:41 +0900

    #02 Django-MariaDB 연결_02

---
+++ HanNune/HanNune/models.py
@@ -0,0 +1,58 @@
+from django.db import models
+from django.conf import settings
+
+class live(models.Model):
+    live_id = models.CharField(max_length=20, null=True)
+    live_name = models.CharField(max_length=100, null=True)
+    live_start_dt = models.DateTimeField(null=True)
+    live_end_dt = models.DateTimeField(null=True)
+    live_stat = models.CharField(max_length=20, null=True)
+    campaign_key = models.CharField(max_length=20, null=True)
+
+class live_chat(models.Model):
+    live_id = models.CharField(max_length=20, null=True)
+    live_chat_dt = models.DateTimeField(null=True)
+    user_uid = models.CharField(max_length=20, null=True)
+    user_id = models.CharField(max_length=50, null=True)
+    user_name = models.CharField(max_length=50, null=True)
+    chat_cont = models.CharField(max_length=200, null=True)
+
+class god(models.Model):
+    god_no = models.CharField(max_length=20, null=True)
+    erp_god_no = models.CharField(max_length=60, null=True)
+    dsgn_grp_no = models.CharField(max_length=60, null=True)
+    god_nm = models.CharField(max_length=500, null=True)
+    prdlst_cd = models.CharField(max_length=60, null=True)
+    prdlst_grp_cd = models.CharField(max_length=60, null=True)
+
+class god_god_evl(models.Model):
+    god_no = models.CharField(max_length=20, null=True)
+    god_evl_turn = models.IntegerField(max_length=10, null=True)
+    dsgn_grp_no = models.CharField(max_length=60, null=True)
+    cstmr_nm = models.CharField(max_length=100, null=True)
+    inflow_ord_sect_cd = models.CharField(max_length=60, null=True)
+    god_evl_sj = models.CharField(max_length=200, null=True)
+    god_evl_cont = models.CharField(max_length=4000, null=True)
+
+class god_god_evl_atch_file(models.Model):
+    god_no = models.CharField(max_length=20, null=True)
+    god_evl_turn = models.IntegerField(max_length=10, null=True)
+    atch_file_turn = models.IntegerField(max_length=10, null=True)
+    atch_file_nm = models.CharField(max_length=300, null=True)
+    atch_file_url = models.CharField(max_length=300, null=True)
+    expect_img_badn_rt = models.IntegerField(max_length=10, null=True)
+    img_rtat_num = models.IntegerField(max_length=10, null=True)
+
+class god_god_evl_size(models.Model):
+    god_no = models.CharField(max_length=20, null=True)
+    god_evl_turn = models.IntegerField(max_length=10, null=True)
+    mbr_size_cd = models.CharField(max_length=60, null=True)
+    size_val = models.CharField(max_length=300, null=True)
+    size_unit_cd = models.CharField(max_length=60, null=True)
+
+class god_god_evl_wear_info(models.Model):
+    god_no = models.CharField(max_length=20, null=True)
+    god_evl_turn = models.IntegerField(max_length=10, null=True)
+    wear_info_sect_cd = models.CharField(max_length=60, null=True)
+    wear_info_sect_detail_cd = models.CharField(max_length=60, null=True)
+    wear_info_sect_detail_val = models.IntegerField(max_length=10, null=True)
