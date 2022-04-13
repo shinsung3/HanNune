961a13d1bc88d88f1e91bace08474e97c375847e
nustar <tlstjd436@naver.com>
2022-04-13 17:19:14 +0900

    #merge - keyword & emotion

---
+++ HanNune/frontend/src/store/emotionDetail.js
@@ -0,0 +1,19 @@
+//store.js
+import Vue from "vue";
+import Vuex from "vuex";
+
+Vue.use(Vuex);
+
+const store = new Vuex.Store({
+    state:{
+        n:101
+    },
+    getters:{
+    },
+    mutations:{
+    },
+    action:{
+    }
+});
+
+export default store
